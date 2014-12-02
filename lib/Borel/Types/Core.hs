eae6165ff63819a92d5a106c281c505f740b7114
Tran Ma <tran@defma.in>
2014-12-02 15:40:37 +1100

    types: rm resourceID

--- lib/Borel/Types/Core.hs
+++ lib/Borel/Types/Core.hs
@@ -20,14 +20,12 @@ module Borel.Types.Core
   , MetricIdentifier
   , mkMetric, streamSummary
     -- * Domain of resources, e.g. OpenStack and SomethingElse (tm)
-  , Domain(..), ResourceID(..)
-  , domainOrigins, domainIdentKey, domainTagKey, domainIdentifiers
-  , Providing(..), Provided(..)
-  , idSearchRequest, idValidate
+  , Domain(..)
+  , domainOrigins, domainIdentKey, domainTagKey
+  , Providing(..)
   ) where
 
 import           Data.Aeson.TH
-import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as B
 import           Data.Map              (Map)
 import qualified Data.Map              as M
@@ -37,7 +35,6 @@ import           Pipes
 import qualified Pipes.Prelude         as P
 import           Web.Scotty            (Parsable)
 
-import           Chevalier.Types
 import           Vaultaire.Types
 import           Borel.Types.Resource
 
@@ -50,15 +47,6 @@ import           Borel.Types.Resource
 --
 data Domain = forall a. Providing a => Domain a
 
--- | A queriable resource instance from a given provider.
---   e.g. a particular CPU from OpenStack
---
---   This is a different concept than @Resource@, which refers to the
---   abstract sense of resources, e.g. "CPU" as in CPU usage in general.
---   This refers to concrete resources such as a particular CPU tenancy in OpenStack.
---
-data ResourceID = forall a. Provided a => ResourceID a
-
 -- | Interface to deal with different source of resources,
 --   e.g. OpenStack and SomethingElse (tm)
 --   indexed by the type of resource sources
@@ -67,33 +55,12 @@ class Providing sauce where
   identKey    :: sauce -> Text             -- ^ FIXME: Mystery
   domainKey   :: sauce -> Text             -- ^ FIXME: Mystery two
   domain      :: ResourceGroup -> sauce    -- ^ Each logical group of resources should be managed by one domain
-  identifiers :: Monad m
-              => sauce -> TimeStamp -> TimeStamp
-              -> Producer ByteString m ()
-              -> Producer ResourceID m (Maybe ErrorMsg)
-  -- ^ Parse some bytestring into resource IDs, e.g. OpenStack tenancy ids
-
-class Provided res where
-  mkSearchRequest :: res -> ResourceGroup -> SourceRequest
-  -- ^ Construct a Chevalier request for this resource
-  --   the mapping between "provided" and the resource type
-  --   is done manually, e.g. if "provided" is an OpenStack
-  --   tenancy, "resource" must be an OpenStack resource.
-  validate :: res -> (TimeStamp, TimeStamp) -> (TimeStamp, TimeStamp)
-  -- ^ The period bounded by the specifed (start, end) in which the
-  --   resource is valid.
 
 -- boilerplate
 
 domainOrigins     (Domain a) = originsOf a
 domainIdentKey    (Domain a) = identKey a
 domainTagKey      (Domain a) = domainKey a
-domainIdentifiers (Domain a) = identifiers a
-
-idSearchRequest (ResourceID a) = mkSearchRequest a
-idValidate      (ResourceID a) = validate a
-
-type ErrorMsg = String
 
 
 -- Intermediate types ----------------------------------------------------------

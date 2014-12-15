7aac36e39f719d5cd4f29a00d988e9e8c31379ca
Oswyn Brent <oswyn.brent@anchor.com.au>
2014-12-15 14:51:35 +1100

    Added tests for consolidated queries

--- tests/Query.hs
+++ tests/Query.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Borel.Aggregate
 import           Control.Monad.Logger
+import           Data.Bits
 import           Data.List            (nub, sort)
+import           Data.Map             (Map)
+import qualified Data.Map             as M
 import           Data.Monoid
+import           Data.Word
 import           Network.URI
 import           Pipes
 import           Pipes.Lift
@@ -12,20 +19,28 @@ import           Borel.Log
 import           Borel.Query
 import           Borel.Types
 import           Marquise.Types
-import           Vaultaire.Query      hiding (metrics)
+import           Vaultaire.Query
 
 main :: IO ()
 main = hspec $ do
   -- safe
+
   describe "Ceilometer aggregation" $ do
     it "processes gauge data" $ pending
     it "processes cumulative data" $ pending
-    it "processes event-based consolidated data" $ pending
-    it "processes pollster-based consolidated data" $ pending
+    it "processes event-based consolidated data" $ do
+        xs <- runTest $ mkQuery volMetrics s e okEventPoints
+        xs `shouldBe` [(volumes, expectedEventPayload)]
+    it "processes pollster-based consolidated data" $ do
+        xs <- runTest $ mkQuery instanceMetrics 0 15 okPollsterPoints
+        let ms = sort $ nub xs
+        ms `shouldBe` expectedInstanceResult
+
+-- = 7 seconds of flavor 2 + 4 seconds of flavor 3
 
   describe "Result of a successful Borel query" $ do
     it "only includes the specified metrics" $ do
-      xs <- runTest $ mkQuery metrics bang doom okGaugePoints
+      xs <- runTest $ mkQuery metrics s e okGaugePoints
       let ms = sort $ nub $ map fst xs
       ms `shouldBe` metrics
 
@@ -35,16 +50,21 @@ main = hspec $ do
           = runSafeT
           $ P.toListM
           $ runLoggerP LevelInfo
-          $ runReaderP (BorelEnv conf mempty bang doom) x
+          $ runReaderP (BorelEnv conf flavorMap s e) x
 
         conf = BackendConfig [] nullURI nullURI
 
-        bang = TimeStamp 1300000000000000000
-        doom = TimeStamp 1400000000000000000
-
         -- a Borel query only operates on metrics of the same group
         metrics = [ cpu ]
+        volMetrics = [ volumes ]
+        instanceMetrics = map computeInstance $ M.elems flavorMap
 
+s, e :: TimeStamp
+s = TimeStamp 1300000000000000000
+e = TimeStamp 1400000000000000000
+sRaw, eRaw :: Word64
+sRaw = unTimeStamp s
+eRaw = unTimeStamp e
 
 okGaugePoints , okCumulativePoints, okEventPoints, okPollsterPoints
   :: Monad m => Query m SimplePoint
@@ -54,7 +74,50 @@ okGaugePoints      = Select $ each
   [ SimplePoint addr (TimeStamp 1378389600000000000) 1
   , SimplePoint addr (TimeStamp 1378389700000000000) 3
   , SimplePoint addr (TimeStamp 1378389800000000000) 5 ]
-  where addr = read "LygHa16AHYF"
 
-okEventPoints      = undefined
-okPollsterPoints   = undefined
+
+addr = read "LygHa16AHYF"
+
+
+-- Volume events
+-- Expected = 2 * 10 + 5 * 30 + 4 * 30 + 10 * 10
+--          = 20 + 150 + 120 + 100
+--          = 390
+okEventPoints      = Select $ each
+  [ SimplePoint addr (TimeStamp sRaw) vPayload1
+  , SimplePoint addr (TimeStamp (sRaw + 2)) vPayload2
+  , SimplePoint addr (TimeStamp (sRaw + 7)) vPayload2
+  , SimplePoint addr (TimeStamp (sRaw + 11)) vPayload1
+  , SimplePoint addr (TimeStamp (sRaw + 21)) vPayloadEnd
+  ]
+
+expectedEventPayload = 390
+
+vPayload1, vPayload2 :: Word64
+vPayload1 = 2 + (1 `shift` 8) + (2 `shift` 16) + (10 `shift` 32)
+vPayload2 = 2 + (1 `shift` 8) + (2 `shift` 16) + (30 `shift` 32)
+vPayloadEnd = 4 + (3 `shift` 8) + (2 `shift` 16) + (30 `shift` 32)
+
+flavorMap :: Map Word64 String
+flavorMap = M.fromList [ (siphash "1", "flavor1")
+                       , (siphash "2", "flavor2")
+                       , (siphash "3", "flavor3")
+                       ]
+
+-- Instance pollsters
+-- Expected 2 seconds of flavor2 + 5 seconds of flavor 2 + 4 seconds of flavor 3 + 2 seconds of flavor 2 + 2 seconds of flavor 2
+-- = 11 seconds of flavor 2 + 4 seconds of flavor 3
+okPollsterPoints   = Select $ each
+  [ SimplePoint addr (TimeStamp 0)  (siphash "2")
+  , SimplePoint addr (TimeStamp 2)  (siphash "2")
+  , SimplePoint addr (TimeStamp 7)  (siphash "3")
+  , SimplePoint addr (TimeStamp 11) (siphash "2")
+  , SimplePoint addr (TimeStamp 13) (siphash "2")
+  -- Query goes up to t = 15 so extra 2 seconds
+  ]
+
+expectedInstanceResult =
+    [ (computeInstance "flavor1", 0)
+    , (computeInstance "flavor2", 11)
+    , (computeInstance "flavor3", 4)
+    ]

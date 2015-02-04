e13c406631afc831edba17acd3e186d159b8660b
Tran Ma <tran@defma.in>
2015-02-04 12:12:51 +1100

    types: nuked domains

--- lib/Borel/Types.hs
+++ lib/Borel/Types.hs
@@ -1,13 +1,9 @@
--- * Types for entities that a Borel query needs to know about,
---   which includes resources (e.g. cpu, memory in openstack),
---   customers, metrics
---
 module Borel.Types
-  ( module Borel.Types.Core
-  , module Borel.Types.Metric
+  ( module Borel.Types.Metric
+  , module Borel.Types.UOM
   , module Borel.Server.Types
   ) where
 
 import           Borel.Server.Types
-import           Borel.Types.Core
 import           Borel.Types.Metric
+import           Borel.Types.UOM

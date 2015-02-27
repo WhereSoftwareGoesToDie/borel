2f50cf97dbad041f284f83f4890b30c32046ee2f
Oswyn Brent <oswyn.brent@anchor.com.au>
2015-02-27 14:01:31 +1100

    Update Env to add instance status filtering predicate

---
+++ lib/Borel/Ceilometer/Instance.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE TupleSections #-}
+
+module Borel.Ceilometer.Instance where
+
+import Ceilometer.Types
+
+billableStatus :: PFInstanceStatus -> Bool
+billableStatus InstanceError            = False
+billableStatus InstanceActive           = True
+billableStatus InstanceShutoff          = False
+billableStatus InstanceBuild            = False
+billableStatus InstanceRebuild          = False
+billableStatus InstanceDeleted          = False
+billableStatus InstanceSoftDeleted      = False
+billableStatus InstanceShelved          = False
+billableStatus InstanceShelvedOffloaded = False
+billableStatus InstanceReboot           = True
+billableStatus InstanceHardReboot       = True
+billableStatus InstancePassword         = True
+billableStatus InstanceResize           = True
+billableStatus InstanceVerifyResize     = True
+billableStatus InstanceRevertResize     = True
+billableStatus InstancePaused           = False
+billableStatus InstanceSuspended        = False
+billableStatus InstanceRescue           = False
+billableStatus InstanceMigrating        = False
